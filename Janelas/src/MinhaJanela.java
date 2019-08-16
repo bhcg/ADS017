2491031a8c05791ae07a05a12cfe81cab366a1f9
18214290016 <18214290016@OLAB04-D18.academico.iesb.br>
2019-08-15 22:36:42 -0300

    Criação da primeira JFrame e JDialog. #3

---
+++ Janelas/src/MinhaJanela.java
@@ -0,0 +1,17 @@
+
+import java.awt.HeadlessException;
+import javax.swing.JFrame;
+
+
+public class MinhaJanela extends JFrame {
+
+    public MinhaJanela() throws HeadlessException {
+        setTitle("Fuck Window");
+        setSize(600, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+    }
+    
+    
+    
+}
