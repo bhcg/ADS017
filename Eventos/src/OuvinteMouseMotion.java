74b07d34f6c134332dd461f1b5b0a7dc6c7125f1
18214290016 <18214290016@OLAB04-D18.academico.iesb.br>
2019-09-12 22:34:21 -0300

    Foi feito alguns eventos de mouse e teclado.

---
+++ Eventos/src/OuvinteMouseMotion.java
@@ -0,0 +1,11 @@
+
+import java.awt.event.MouseMotionListener;
+
+public class OuvinteMouseMotion implements MouseMotionListener {
+    
+    public void mouseDragged(MouseEvent e) {
+    
+    
+    }
+    
+}
