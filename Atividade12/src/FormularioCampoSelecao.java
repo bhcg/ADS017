8c72542566243b71b5d491dc72d7a4fad7d3adae
18214290016 <18214290016@OLAB04-D18.academico.iesb.br>
2019-10-24 22:11:14 -0300

    Atividade 12...

---
+++ Atividade12/src/FormularioCampoSelecao.java
@@ -0,0 +1,17 @@
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JList;
+
+public class FormularioCampoSelecao extends JFrame {
+        JComboBox comboUF = new JComboBox();
+        JList listaMunincipio = new JList();
+        String[] arrayUF;
+        String [][] arrayMunicipio; 
+        
+       public static void main(String[] args) {
+        
+       
+         
+    }
+        
+}
